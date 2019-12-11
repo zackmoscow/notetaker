c70ee250fa7be5b30d8312b6e2285b9af745920c
Zack Moscow <zmoscow@gmail.com>
2019-12-10 19:11:50 -0600

    added public folder for static files

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ let currentID = 0;
 // HTML ROUTES
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
   
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 // API ROUTES
